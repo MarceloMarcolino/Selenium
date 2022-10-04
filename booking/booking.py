9275009e6413a0a6a48aefc067887215b6462ce0
Marcelo Antonio Pereira Marcolino <marcelo.marcolino@fatec.sp.gov.br>
2022-10-04 12:11:01 -0300

    Deal Searching Part 1

--- booking/booking.py
+++ booking/booking.py
@@ -1,6 +1,7 @@
 import booking.constants as const
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class Booking(webdriver.Chrome):
 	def __init__(self, driver_path=os.getcwd(), teardown=False):
@@ -8,6 +9,8 @@ class Booking(webdriver.Chrome):
 		self.teardown = teardown
 		os.environ['PATH'] += self.driver_path
 		super(Booking, self).__init__()
+		self.implicitly_wait(15)
+		self.maximize_window()
 
 	def __exit__(self, exc_type, exc_val, exc_tb):
 		if self.teardown:
@@ -15,3 +18,17 @@ class Booking(webdriver.Chrome):
 
 	def land_first_page(self):
 		self.get(const.BASE_URL)
+
+	def change_currency(self, currency=None):
+		currency_element = self.find_element(By.CSS_SELECTOR, 'button[data-modal-header-async-type="currencyDesktop"]')
+		currency_element.click()
+		selected_currency_element =self.find_element(By.CSS_SELECTOR, f'a[data-modal-header-async-url-param*="selected_currency={currency}"]')
+		selected_currency_element.click()
+
+	def select_place_to_go(self, place_to_go):
+		search_field = self.find_element(By.ID, 'ss')
+		search_field.clear()
+		search_field.send_keys(place_to_go)
+
+		first_result = self.find_element(By.CSS_SELECTOR, 'li[data-i="0"]')
+		first_result.click()
--- run.py
+++ run.py
@@ -2,9 +2,5 @@ from booking.booking import Booking
 
 with Booking() as bot:
 	bot.land_first_page()
-
-'''#If I want to close the browser after running the code, use this one
-with Booking(teardown=True) as bot:
-	bot.land_first_page()
-	print('Exiting ...')
-'''
+	bot.change_currency(currency='BRL')
+	bot.select_place_to_go('New York')
