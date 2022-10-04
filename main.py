b47be34043f8481d55e3b141dec68705d6249d46
Marcelo Antonio Pereira Marcolino <marcelo.marcolino@fatec.sp.gov.br>
2022-10-03 22:11:26 -0300

    Sending Keys & CSS Selector

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 os.environ['PATH'] += os.getcwd()
 driver = webdriver.Chrome()
-driver.get("https://www.w3schools.com/howto/howto_js_popup.asp")
-driver.implicitly_wait(8)
-my_element = driver.find_element(By.CLASS_NAME, 'popup')
-my_element.click()
 
-WebDriverWait(driver, 30).until(
-	EC.text_to_be_present_in_element(
-		(By.ID, 'myPopup') , # Element filtration
-		'A Simple Popup!'# The expected text
-	)
-)
+driver.get('https://syntaxprojects.com/basic-first-form-demo.php') #sum test
+#driver.get('https://www.folha.uol.com.br') #no_button test
+driver.implicitly_wait(5)
+try:
+	no_button = driver.find_element(By.CLASS_NAME, 'banner-lgpd-consent__accept')
+	no_button.click()
+except:
+	print('No element with this class name. Skipping ....')
+
+sum1 = driver.find_element(By.ID, 'sum1')
+sum2 = driver.find_element(By.ID, 'sum2')
+
+sum1.send_keys(Keys.NUMPAD1, Keys.NUMPAD5 )
+sum2.send_keys(15)
+
+btn = driver.find_element(By.CSS_SELECTOR, 'button[onclick="return total()"]')
+btn.click()
