63360c4a98a4817a7034ac279600d20ab4cebef8
Marcelo Antonio Pereira Marcolino <marcelo.marcolino@fatec.sp.gov.br>
2022-10-03 21:41:18 -0300

    Explicit vs Implicit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 os.environ['PATH'] += os.getcwd()
 driver = webdriver.Chrome()
-driver.get("https://www.w3schools.com/howto/howto_css_download_button.asp")
-driver.implicitly_wait(30) #time.sleep(30)
-my_element = driver.find_element(By.CLASS_NAME, 'w3-right')
+driver.get("https://www.w3schools.com/howto/howto_js_popup.asp")
+driver.implicitly_wait(8)
+my_element = driver.find_element(By.CLASS_NAME, 'popup')
 my_element.click()
+
+WebDriverWait(driver, 30).until(
+	EC.text_to_be_present_in_element(
+		(By.ID, 'myPopup') , # Element filtration
+		'A Simple Popup!'# The expected text
+	)
+)
