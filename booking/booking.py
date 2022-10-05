3b73ff9b9300b860dad19fa7dcb74986abe442f5
Marcelo Antonio Pereira Marcolino <marcelo.marcolino@fatec.sp.gov.br>
2022-10-04 22:01:20 -0300

    Deal Searching Part 2

--- booking/booking.py
+++ booking/booking.py
@@ -19,10 +19,16 @@ class Booking(webdriver.Chrome):
 	def land_first_page(self):
 		self.get(const.BASE_URL)
 
+	def change_language(self, language=None):
+		language_element = self.find_element(By.CSS_SELECTOR, 'button[data-modal-id="language-selection"]')
+		language_element.click()
+		selected_language_element = self.find_element(By.CSS_SELECTOR, f'a[data-lang="{language}"]')
+		selected_language_element.click()
+
 	def change_currency(self, currency=None):
 		currency_element = self.find_element(By.CSS_SELECTOR, 'button[data-modal-header-async-type="currencyDesktop"]')
 		currency_element.click()
-		selected_currency_element =self.find_element(By.CSS_SELECTOR, f'a[data-modal-header-async-url-param*="selected_currency={currency}"]')
+		selected_currency_element = self.find_element(By.CSS_SELECTOR, f'a[data-modal-header-async-url-param*="selected_currency={currency}"]')
 		selected_currency_element.click()
 
 	def select_place_to_go(self, place_to_go):
@@ -32,3 +38,32 @@ class Booking(webdriver.Chrome):
 
 		first_result = self.find_element(By.CSS_SELECTOR, 'li[data-i="0"]')
 		first_result.click()
+
+	def select_dates(self, check_in_date, check_out_date):
+		check_in_element = self.find_element(By.CSS_SELECTOR, f'td[data-date="{check_in_date}"]')
+		check_in_element.click()
+		check_out_element = self.find_element(By.CSS_SELECTOR, f'td[data-date="{check_out_date}"]')
+		check_out_element.click()
+
+	def select_adults(self, count=1):
+		selection_element = self.find_element(By.ID, 'xp__guests__toggle')
+		selection_element.click()
+
+		while True:
+			decrease_adults_element = self.find_element(By.CSS_SELECTOR, 'button[aria-label="Diminuir número de Adultos"]')
+			decrease_adults_element.click()
+			#If the value of adults reaches 1, then we should get out of the while loop
+			adults_value_element = self.find_element(By.ID, 'group_adults')
+			adults_value = adults_value_element.get_attribute('value') # Should give back the adults count
+
+			if int(adults_value) == 1:
+				break
+
+		increase_button_element = self.find_element(By.CSS_SELECTOR, 'button[aria-label="Aumentar número de Adultos"]')
+
+		for _ in range(count - 1):
+			increase_button_element.click()
+
+	def click_search(self):
+		search_button = self.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
+		search_button.click()
--- run.py
+++ run.py
@@ -2,5 +2,9 @@ from booking.booking import Booking
 
 with Booking() as bot:
 	bot.land_first_page()
+	bot.change_language(language='pt-br')
 	bot.change_currency(currency='BRL')
 	bot.select_place_to_go('New York')
+	bot.select_dates(check_in_date='2022-10-19', check_out_date='2022-10-25')
+	bot.select_adults(10)
+	bot.click_search()
